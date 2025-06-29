6d2456ba73b9ea95aaf43a1be32fedf968c0b202
YuriSousaAlmeida <127100378+YuriSousaAlmeida@users.noreply.github.com>
2025-06-29 02:20:11 -0300

    Add files via upload

--- Sistema.java
+++ Sistema.java
@@ -1,2 +1,26 @@
 public class Sistema {
+    public int qtd;
+    public void cadastrarProdutoEletronico(){
+
+    }
+
+    public void cadastrarProdutoAlimenticio(){
+
+    }
+
+    public String listarProdutos(){
+
+    }
+
+    public void realizarVenda(){
+
+    }
+
+    public String listarVendas(){
+
+    }
+
+    public void pesquisarProduto(){
+
+    }
 }
